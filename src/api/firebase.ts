a3e83b23111ad447a7bfd704eef197ff24667a69
god-of-js <henryeze019@gmail.com>
2023-10-16 09:23:30 +0100

    ci(): redeploy

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -3,7 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_APP_ID, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_MEASUREMENT_ID, VITE_FIREBASE_MESSAGING_SENDER_ID, VITE_FIREBASE_PROJECT_ID, VITE_FIREBASE_STORAGE_BUCKET } from '../config';
 
-
 const firebaseConfig = {
   apiKey: VITE_FIREBASE_API_KEY,
   authDomain: VITE_FIREBASE_AUTH_DOMAIN,
